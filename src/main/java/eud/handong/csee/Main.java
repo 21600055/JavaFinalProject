03bb8705e3519a68ab08767dcde4d21e60a14a27
user <user@DESKTOP-BGIC6EH>
2019-06-17 01:13:37 +0900

    commit

--- src/main/java/eud/handong/csee/Main.java
+++ src/main/java/eud/handong/csee/Main.java
@@ -16,13 +16,41 @@ public class Main {
 	public static void main(String[] args)
 	{
 		Main main=new Main();
+		main.run(args);
 	}
 	
 	public void run(String[] args)
 	{
-		Options option=createOption();
+		Options options=createOption();
+		
+		if(parseOptions(options,args))
+		{
+			
+		}
+	}
+	
+	private boolean parseOptions(Options options,String[] args)
+	{
 		
 	}
 	
+	private Options createOprions()
+	{
+		Options options=new Options();
+		
+		options.addOption((Option.builder("i").longOpt("input")
+				           .desc("this is input name and reqired")
+				           .hasArg()
+				           .argname("input path")
+				           .required()
+				           .build());
+	}
 	
+	private void printHelp(Options options)
+	{
+		HelpFormatter formatter=new HelpFormatter();
+		String header = "Java Final Project";
+		String footer ="";
+		formatter.printHelp("Java Final Project", header, options, footer, true);
+	}
 }
