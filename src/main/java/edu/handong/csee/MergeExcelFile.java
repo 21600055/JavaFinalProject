0754133e2de5e19f7571b74824fc738b25135f04
user <user@192.168.2.19>
2019-06-17 07:30:26 +0900

    commit

--- src/main/java/edu/handong/csee/MergeExcelFile.java
+++ src/main/java/edu/handong/csee/MergeExcelFile.java
@@ -1,10 +1,18 @@
 package edu.handong.csee;
 
+import edu.handong.csee.utils.*;
 
 public class MergeExcelFile {
 	
 	public void run(String[] args)
 	{
+		try {
+			if(args.length<2)
+				throw new NotEnoughArgumentException();
+		} catch (NotEnoughArgumentException e) {
+			System.out.println(e.getMessage());	
+			System.exit(0);
+		}
 		
 	}
 }
