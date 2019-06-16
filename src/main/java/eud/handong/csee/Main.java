83132c3ee0f29701f76258f228901d77e4337873
user <user@DESKTOP-BGIC6EH>
2019-06-16 18:07:29 +0900

    commit

--- src/main/java/eud/handong/csee/Main.java
+++ src/main/java/eud/handong/csee/Main.java
@@ -1,5 +1,28 @@
 package eud.handong.csee;
 
-public class Main {
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
+public class Main {
+	
+	String input;
+	String output;
+	boolean help;
+	
+	public static void main(String[] args)
+	{
+		Main main=new Main();
+	}
+	
+	public void run(String[] args)
+	{
+		Options option=createOption();
+		
+	}
+	
+	
 }
